0341ca936a630b223636b3a4ea69233117f9e668
Witold Tafel <witold.tafel@gmail.com>
2018-03-12 00:57:37 +0100

    name and title changes to meet the reqs

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ app = Flask(__name__)
 @app.route('/list')
 @app.route('/')
 def route_index():
-    return render_template('index.html', stories = list_of_dic_from_file() )
+    return render_template('list.html', stories = list_of_dic_from_file() )
     
 @app.route('/list',methods=['POST'])
 @app.route('/', methods=['POST'])
 def del_row():
     del_row_in_file(int(list(request.form.values())[-1]))
-    return render_template('index.html', stories = list_of_dic_from_file())
+    return render_template('list.html', stories = list_of_dic_from_file())
 
 @app.route('/story')
 def story_add():
-    return render_template('story.html')
+    return render_template('form.html')
 
 @app.route('/story', methods=['POST'])
 def route_save():
@@ -26,7 +26,7 @@ def route_save():
 @app.route('/story/<story_id>')
 def story_edit(story_id):
     dict = list_of_dic_from_file()[int(story_id)-1]
-    return render_template('story.html',dict=dict)
+    return render_template('form.html',dict=dict)
 
 
 @app.route('/story/<story_id>', methods=['POST'])
@@ -38,26 +38,26 @@ def save_edit(story_id):
 
 
 def write_form_to_file():
-    with open('/home/wiciu/codecool/web/super3000/data.csv', 'a') as f: 
+    with open('data.csv', 'a') as f: 
         w = csv.DictWriter(f, sorted(session['story'].keys()))
         w.writerow(session['story'])
 
 def replace_row_in_file(row_number):
     list_dict = list_of_dic_from_file()
     list_dict[row_number-1] = session['story']
-    with open('/home/wiciu/codecool/web/super3000/data.csv', 'w') as f: 
+    with open('data.csv', 'w') as f: 
         w = csv.DictWriter(f, sorted(session['story'].keys()))
         w.writerows(list_dict)
 
 
 def list_of_dic_from_file():
     try:
-        with open("/home/wiciu/codecool/web/super3000/data.csv") as f:
+        with open("data.csv") as f:
             reader = csv.DictReader(f,sorted(session['story'].keys()))
             dics = [ d for d in reader ]
             return dics
     except FileNotFoundError:
-        with open("/home/wiciu/codecool/web/super3000/data.csv", "a") as f:
+        with open("data.csv", "w+") as f:
             reader = csv.DictReader(f,sorted(session['story'].keys()))
             dics = [ d for d in reader ]
             return dics
@@ -65,7 +65,7 @@ def list_of_dic_from_file():
 def del_row_in_file(row_number):
     list_dict = list_of_dic_from_file()
     del list_dict[row_number] 
-    with open('/home/wiciu/codecool/web/super3000/data.csv', 'w') as f: 
+    with open('data.csv', 'w') as f: 
         w = csv.DictWriter(f, sorted(session['story'].keys()))
         w.writerows(list_dict)
 
